1d0e30aa08554860a7a44b399315c0c16cc5d0aa
zxg-com <342509492@qq.com>
2018-10-18 16:56:41 +0800

    po分层优化，根据page。driver来实现 一些需要登录的，直接引入登录页直接使用即可，非常方便

--- src/test_case/test_appium.py
+++ src/test_case/test_appium.py
@@ -5,47 +5,33 @@ description:测试登录和退出功能
 '''
 import unittest
 
-from src.pages import login_page
-from src.common import gesture_mainpulation
-from src.common import driver_configure
+from src.pages import login_page,initpage
+
 
 
 class test_appium(unittest.TestCase):
+    case_name = "正常登录"
     @classmethod
     def setUpClass(cls):
-        dconfigur = driver_configure.driver_configure()
-        cls.driver = dconfigur.get_driver()
-        cls.GM = gesture_mainpulation.gesture_mainpulation()
+        #启动页
+        cls.page = initpage.initPage()
+
 
     def test_01login(self):
-        '''测试登录功能'''
-        # 主页面
-        #self.index_page = index_page.index_page(self.driver)
-        self.index_page.click_btnCancel()
-        self.GM.swipe_down(self.driver)
-        self.index_page.click_inXSTeach()
-        # 我在邢帅
-        #self.myInfo_page = myInfo_page.myInfo_page(self.driver)
-        self.myInfo_page.click_login_btn()
-        # 登录页
-        self.login_page = login_page.login_page(self.driver)
-        self.login_page.input_user("lihailing@xsteqach.com")
-        self.login_page.input_Pws("123456")
-        self.login_page.click_btnLogin()
-        self.assertEqual(u'签到',self.myInfo_page.getText_signHint())
 
-    def test_02loginOut(self):
-        '''测试退出功能'''
-        #self.myInfo_page = myInfo_page.myInfo_page(self.driver)
-        self.myInfo_page.click_relative()
-        #self.relative = relative_page.relative_page(self.driver)
-        self.relative.click_tvLoginOut()
-        self.relative.click_btnYes()
-        self.assertEqual(u'点击登录',self.myInfo_page.getText_login())
+        self.page.always_allow()
+        self.page.click_skip()
+        '''测试登录功能'''
+        # 登录页面
+        self.page=login_page.login_page(self.page)
+        self.page.click_zhanghaotab()
+        self.page.input_user('15011270128')
+        self.page.input_Pws('111111')
+        self.page.click_login()
 
     @classmethod
     def tearDownClass(cls):
-        cls.driver.quit()
+        cls.page.quit()
 
 if __name__=='__main__':
     unittest.main()
